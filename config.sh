9fade8c602f8da3e7bef67a1f994d2ebd3b75378
Nate Lao <lao.nathan@yahoo.com>
2020-03-29 23:27:43 -0400

    added base configuration

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##############################################################################
+# File: config.sh
+# Author: Nate Lao (nlao1@jh.edu)
+# Created On: 3/29/2020
+# Description:
+#       This contains the git TAGs for each subsystem for Clueless to be added
+#       in the master build.
+#       For building, this revision must be tagged and pushed to origin.
+#
+#       NOTE: ALL tags must match Debian versioning schema:
+#       <major>.<minor>-revision
+##############################################################################
+
+TAG_FRONTEND=0.0-0
+TAG_SERVERSIDE=0.0-0
+TAG_BACKEND=0.0-0
