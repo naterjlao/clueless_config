cb715d06f76d42573b85ff1343ff90c1cfd57e40
Nate Lao <lao.nathan@yahoo.com>
2020-03-30 22:53:59 -0400

    0.1-0 update to restructuring efforts

--- config.sh
+++ config.sh
@@ -12,6 +12,6 @@
 #       <major>.<minor>-revision
 ##############################################################################
 
-TAG_FRONTEND=restructure1
-TAG_SERVERSIDE=restructure1
-TAG_BACKEND=0.0-0
+TAG_FRONTEND=0.1-0
+TAG_SERVERSIDE=0.1-0
+TAG_BACKEND=0.1-0
